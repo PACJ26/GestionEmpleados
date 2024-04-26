84fb4f6b99e7630b73f92a9a73f9fcaa0ba93699
PACJ26 <juan123alv@gmail.com>
2024-04-26 18:16:31 -0500

    no terminado

---
+++ AppGestion/src/igu/formLogin.java
@@ -0,0 +1,217 @@
+package igu;
+
+import Utilidad.Mensajes;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+import persistencia.Conexion;
+
+public class formLogin extends javax.swing.JFrame {
+
+    Conexion conexion;
+
+    public formLogin() {
+        conexion = new Conexion();
+        this.setTitle("Inicio de Sesion");
+        this.setLocationRelativeTo(null);
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        btnIniciarSesion = new javax.swing.JButton();
+        txtDocumento = new javax.swing.JTextField();
+        txtClave = new javax.swing.JPasswordField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        btnIniciarSesion.setText("Iniciar Sesion");
+        btnIniciarSesion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIniciarSesionActionPerformed(evt);
+            }
+        });
+
+        txtDocumento.setText("jTextField1");
+
+        txtClave.setText("jPasswordField1");
+
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("Ingrese Contraseña");
+
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("Ingrese Numero de Documento");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(490, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(38, 38, 38)
+                                .addComponent(btnIniciarSesion))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(23, 23, 23)))
+                        .addGap(54, 54, 54))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(txtDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(67, 67, 67))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(txtClave, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(67, 67, 67))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(115, 115, 115)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtClave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(btnIniciarSesion)
+                .addContainerGap(192, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnIniciarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIniciarSesionActionPerformed
+        // TODO add your handling code here:
+
+        String documento = txtDocumento.getText();
+        String clave = new String(txtClave.getPassword());
+
+        if (documento.isEmpty() || clave.isEmpty()) {
+            Mensajes.mostrarAdvertencia("Hay Campos vacios");
+            return;
+        }
+
+        try (Connection con = conexion.conectar()) {
+
+            CallableStatement calls = con.prepareCall("{CALL IniciarSesion(?, ?, ?)}");
+            calls.setString(1, documento);
+            calls.setString(2, clave);
+            calls.registerOutParameter(3, Types.VARCHAR);
+            calls.execute();
+            
+            String mensaje = calls.getString(3);
+
+            if (mensaje.equals("Usuario no encontrado")) {
+
+                Mensajes.mostrarError(mensaje);
+
+            } else if (mensaje.equals("Clave incorrecta")) {
+
+                Mensajes.mostrarError(mensaje);
+
+            } else if (mensaje.equals("Inicio de sesión exitoso")) {
+
+                Mensajes.mostrarExito(mensaje);
+                int cargoEmpleado = obtenerCargoEmpleado(documento);
+
+                switch (cargoEmpleado) {
+                    case 2:
+                        formAdministrador administrador = new formAdministrador();
+                        administrador.setVisible(true);
+                        this.dispose();
+                        break;
+                    default:
+                        Mensajes.mostrarError("Cargo no encontrado: Error al iniciar sesion");
+                }
+            }
+            conexion.desconectar();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_btnIniciarSesionActionPerformed
+
+    private int obtenerCargoEmpleado(String documento) throws SQLException {
+        int cargo = -1;
+        try (Connection con = conexion.conectar()) {
+            CallableStatement cs = con.prepareCall("{CALL ObtenerCargoEmpleado(?, ?)}");
+            cs.setString(1, documento);
+            cs.registerOutParameter(2, Types.INTEGER);
+            cs.execute();
+            cargo = cs.getInt(2);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return cargo;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(formLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(formLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(formLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(formLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new formLogin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnIniciarSesion;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField txtClave;
+    private javax.swing.JTextField txtDocumento;
+    // End of variables declaration//GEN-END:variables
+}
--- AppGestion/src/igu/formPrincipal.java
+++ AppGestion/src/igu/formPrincipal.java
@@ -3,11 +3,9 @@ package igu;
 import Utilidad.Mensajes;
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.util.ArrayList;
-import javax.swing.JOptionPane;
 import logica.RegistroEntradaSalida;
 import persistencia.Conexion;
 
@@ -20,7 +18,10 @@ public class formPrincipal extends javax.swing.JFrame {
     public formPrincipal() {
         conexion = new Conexion();
         listaRegistros = new ArrayList();
+        this.setTitle("Gestion Empleados");
+        this.setLocationRelativeTo(null);
         initComponents();
+       
     }
 
     @SuppressWarnings("unchecked")
@@ -28,67 +29,132 @@ public class formPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        Panel_Izquierda = new javax.swing.JPanel();
+        Panel_Derecha = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        txtDocumento = new javax.swing.JTextField();
         btnEntrada = new javax.swing.JButton();
         btnSalida = new javax.swing.JButton();
-        txtDocumento = new javax.swing.JTextField();
+        BtIniciarSesion = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(null);
 
-        btnEntrada.setText("jButton1");
+        Panel_Izquierda.setBackground(new java.awt.Color(0, 102, 102));
+        Panel_Izquierda.setPreferredSize(new java.awt.Dimension(400, 500));
+
+        javax.swing.GroupLayout Panel_IzquierdaLayout = new javax.swing.GroupLayout(Panel_Izquierda);
+        Panel_Izquierda.setLayout(Panel_IzquierdaLayout);
+        Panel_IzquierdaLayout.setHorizontalGroup(
+            Panel_IzquierdaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 360, Short.MAX_VALUE)
+        );
+        Panel_IzquierdaLayout.setVerticalGroup(
+            Panel_IzquierdaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 480, Short.MAX_VALUE)
+        );
+
+        jPanel1.add(Panel_Izquierda);
+        Panel_Izquierda.setBounds(0, 0, 360, 480);
+
+        Panel_Derecha.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 30)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 102, 102));
+        jLabel2.setText("INGRESO LABORAL");
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jLabel1.setText("Digite el Documento de Identidad:");
+
+        btnEntrada.setBackground(new java.awt.Color(0, 102, 26));
+        btnEntrada.setForeground(new java.awt.Color(255, 255, 255));
+        btnEntrada.setText("Empezar Hora Laboral");
         btnEntrada.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnEntradaActionPerformed(evt);
             }
         });
 
-        btnSalida.setText("jButton2");
+        btnSalida.setBackground(new java.awt.Color(102, 0, 0));
+        btnSalida.setForeground(new java.awt.Color(255, 255, 255));
+        btnSalida.setText("Terminar Hora Laboral");
         btnSalida.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnSalidaActionPerformed(evt);
             }
         });
 
-        txtDocumento.setText("jTextField1");
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(176, 176, 176)
-                        .addComponent(btnEntrada)
-                        .addGap(80, 80, 80)
-                        .addComponent(btnSalida))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(258, 258, 258)
-                        .addComponent(txtDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(310, Short.MAX_VALUE))
+        javax.swing.GroupLayout Panel_DerechaLayout = new javax.swing.GroupLayout(Panel_Derecha);
+        Panel_Derecha.setLayout(Panel_DerechaLayout);
+        Panel_DerechaLayout.setHorizontalGroup(
+            Panel_DerechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Panel_DerechaLayout.createSequentialGroup()
+                .addGroup(Panel_DerechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(Panel_DerechaLayout.createSequentialGroup()
+                        .addGap(31, 31, 31)
+                        .addGroup(Panel_DerechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(Panel_DerechaLayout.createSequentialGroup()
+                                .addGap(39, 39, 39)
+                                .addGroup(Panel_DerechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(btnEntrada, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(btnSalida, javax.swing.GroupLayout.PREFERRED_SIZE, 190, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(jLabel2)))
+                    .addGroup(Panel_DerechaLayout.createSequentialGroup()
+                        .addGap(54, 54, 54)
+                        .addGroup(Panel_DerechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, 229, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(86, Short.MAX_VALUE))
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(191, Short.MAX_VALUE)
-                .addComponent(txtDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        Panel_DerechaLayout.setVerticalGroup(
+            Panel_DerechaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Panel_DerechaLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addGap(36, 36, 36)
+                .addComponent(jLabel1)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnEntrada)
-                    .addComponent(btnSalida))
-                .addGap(215, 215, 215))
+                .addComponent(txtDocumento, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(51, 51, 51)
+                .addComponent(btnEntrada, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(43, 43, 43)
+                .addComponent(btnSalida, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(77, Short.MAX_VALUE))
         );
+
+        jPanel1.add(Panel_Derecha);
+        Panel_Derecha.setBounds(360, 0, 390, 380);
+
+        BtIniciarSesion.setBackground(new java.awt.Color(204, 204, 204));
+        BtIniciarSesion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/ICONOS/Logo_Login.png"))); // NOI18N
+        BtIniciarSesion.setText("LOGIN");
+        BtIniciarSesion.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 0));
+        BtIniciarSesion.setBorderPainted(false);
+        BtIniciarSesion.setContentAreaFilled(false);
+        BtIniciarSesion.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        BtIniciarSesion.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        BtIniciarSesion.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        BtIniciarSesion.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        BtIniciarSesion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtIniciarSesionActionPerformed(evt);
+            }
+        });
+        jPanel1.add(BtIniciarSesion);
+        BtIniciarSesion.setBounds(650, 415, 70, 60);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 720, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
         );
 
         pack();
@@ -139,6 +205,12 @@ public class formPrincipal extends javax.swing.JFrame {
             Mensajes.mostrarAdvertencia("Hay Campos vacios");
         }
     }//GEN-LAST:event_btnSalidaActionPerformed
+
+    private void BtIniciarSesionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtIniciarSesionActionPerformed
+        formLogin login = new formLogin();
+        login.setVisible(true);
+       this.dispose();
+    }//GEN-LAST:event_BtIniciarSesionActionPerformed
 
     public String insertarRegistroEntrada(String documento, java.sql.Date fecha, java.sql.Time horaEntrada) {
         String mensaje = "";
@@ -228,8 +300,13 @@ public class formPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtIniciarSesion;
+    private javax.swing.JPanel Panel_Derecha;
+    private javax.swing.JPanel Panel_Izquierda;
     private javax.swing.JButton btnEntrada;
     private javax.swing.JButton btnSalida;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField txtDocumento;
     // End of variables declaration//GEN-END:variables
